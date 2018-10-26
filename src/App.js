786a9dcf3bcb2c767bc0ef0411796630e17b2ece
Tyler Floyd <inglorious_rex@live.com>
2018-10-25 20:21:51 -0700

    Classwork

--- src/App.js
+++ src/App.js
@@ -5,15 +5,41 @@ import { connect } from 'react-redux';
 import { doTest } from './redux/actions';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      color: 'black',
+      banner: 'hello',
+      isOpen: false,
+    };
+    this.buttonHandler = this.buttonHandler.bind(this);
+  }
+
+  buttonHandler(){
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
   render() {
+    let variable = "TESTTTTT";
+    let myBanner;
+
+    if(this.state.isOpen){
+      myBanner = <Header banner={this.state.banner}/>
+    }
     return (
+
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <h1> {variable} </h1>
+            {myBanner}
           </p>
-          <h2>413 Starter Kit </h2>
+           <input value = {this.state.banner} onChange={this.textHandler}/>
+          <button onClick={this.buttonHandler}>Click Here</button>
         </header>
       </div>
     );
